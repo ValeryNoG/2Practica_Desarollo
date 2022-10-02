d5ad2b4c4cf2b9cdeadbca9bf61017dc8032938c
ValeryNoG <vnoguera10969@gmail.com>
2022-10-02 14:41:35 -0600

    Valery Noguera Jimenez Inicio de Practica 2

---
+++ src/main/java/Practica_Desarollo2/PracticaDesarollo2Application.java
@@ -0,0 +1,13 @@
+package Practica_Desarollo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaDesarollo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaDesarollo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/Practica_Desarollo2/PracticaDesarollo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package Practica_Desarollo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaDesarollo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
